b8659d254eb52cdf6aa186a6465c50701cb31d66
smwhitt <smw81@duke.edu>
2018-10-13 20:07:31 -0400

    calendar

--- app/src/main/java/e/smwhitt/myco2/Log.java
+++ app/src/main/java/e/smwhitt/myco2/Log.java
@@ -1,11 +1,19 @@
 package e.smwhitt.myco2;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
+
+import com.alamkanak.weekview.MonthLoader;
+import com.alamkanak.weekview.WeekView;
+import com.alamkanak.weekview.WeekViewEvent;
+
+import java.util.List;
 
 public class Log extends AppCompatActivity {
 
@@ -18,13 +26,13 @@ public class Log extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                    mTextMessage.setText("Calendar");
                     return true;
                 case R.id.navigation_dashboard:
                     mTextMessage.setText(R.string.title_dashboard);
                     return true;
                 case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                    startActivity(new Intent(Log.this, SettingsActivity.class));
                     return true;
             }
             return false;
@@ -39,6 +47,27 @@ public class Log extends AppCompatActivity {
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-    }
 
+//        MonthLoader.MonthChangeListener mMonthChangeListener = new MonthLoader.MonthChangeListener() {
+//            @Override
+//            public List<WeekViewEvent> onMonthChange(int newYear, int newMonth) {
+//                // Populate the week view with some events.
+//                List<WeekViewEvent> events = getEvents(newYear, newMonth);
+//                return events;
+//            }
+//        };
+//
+//        // Get a reference for the week view in the layout.
+//        mWeekView = (WeekView) findViewById(R.id.weekView);
+//
+//        // Set an action when any event is clicked.
+//        mWeekView.setOnEventClickListener(mEventClickListener);
+//
+//        // The week view has infinite scrolling horizontally. We have to provide the events of a
+//        // month every time the month changes on the week view.
+//        mWeekView.setMonthChangeListener(mMonthChangeListener);
+//
+//        // Set long press listener for events.
+//        mWeekView.setEventLongPressListener(mEventLongPressListener);
+    }
 }
